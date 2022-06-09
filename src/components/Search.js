f627bcdaa32ba1c1f7802314cb913142cf68e155
ramiro13 <ramiroperezsanz13@gmail.com>
2022-06-09 11:30:39 +0200

    update

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Search() {
+    return (
+        <>
+            <div className="contenedor">
+                <h4>Buscador</h4>
+                <input></input>
+            </div>
+        </>
+    );
+}
+
+export default Search;
